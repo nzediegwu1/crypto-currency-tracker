64e6547a33cd40e8e3ad6222c9d114e896d03c52
anaeze <nzediegwu1@gmail.com>
2019-07-13 12:06:03 +0100

    - fix log file path error in production

--- src/controllers/logs.js
+++ src/controllers/logs.js
@@ -2,8 +2,14 @@ import path from 'path';
 import fs from 'fs';
 import { response } from '../helpers';
 
+require('dotenv').config();
+
+const { NODE_ENV } = process.env;
+
+const logPath = NODE_ENV === 'production' ? '../../../combined.log' : '../../combined.log';
+
 export default (req, res) =>
-  fs.readFile(path.join(__dirname, '../../combined.log'), 'utf-8', (err, file) => {
+  fs.readFile(path.join(__dirname, logPath), 'utf-8', (err, file) => {
     const lines = file.split('\n');
     lines.pop();
     const message = 'log data from winston';
